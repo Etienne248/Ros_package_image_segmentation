95031ae8b2d3a2d880eaa6a59c23f198faf55a94
Etienne <etienne.pommel@gmail.com>
2023-03-26 12:14:54 +0200

    first commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/workspace/src/Ros_package_image_segmentation/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/workspace/src/Ros_package_image_segmentation/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/workspace/src/Ros_package_image_segmentation/catkin_ws/devel;/opt/ros/noetic' != '' else []
